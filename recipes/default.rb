2d5eb7beca90cb1f1d900f71663beb59e4e32dcf
Graeme Mathieson <mathie@woss.name>
2011-01-23 15:42:55 +0000

    Initial extraction from my own Chef repo.

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+execute "install homebrew" do
+  command "curl -sfL https://github.com/mxcl/homebrew/tarball/master | tar zx -m --strip 1"
+  cwd "/usr/local"
+  not_if { File.exist? '/usr/local/bin/brew' }
+end
+
+package 'git'
+
+execute "update homebrew from github" do
+  command "/usr/local/bin/brew update"
+end
