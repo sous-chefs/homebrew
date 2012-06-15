9d8faa302ab7fd840d12186405e88927adddd283
jtimberman <joshua@opscode.com>
2012-06-15 08:33:27 -0600

    The README.rdoc is converted to README.md

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Install Homebrew and use it as your package provider in Mac OS X"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.1"
 recipe           "homebrew", "Install Homebrew"
 supports         "mac_os_x"
