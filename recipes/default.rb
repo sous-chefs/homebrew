2041d0b8d644ac4e82e4e3f794d2fba6dd095b47
jtimberman <joshua@opscode.com>
2012-10-20 22:36:44 -0600

    [COOK-1793] - use homebrew go script to install

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,17 @@
-execute "install homebrew" do
-  command "curl -sfL https://github.com/mxcl/homebrew/tarball/master | tar zx -m --strip 1"
-  cwd "/usr/local"
+homebrew_go = "#{Chef::Config[:file_cache_path]}/homebrew_go"
+
+remote_file homebrew_go do
+  source "https://raw.github.com/mxcl/homebrew/go"
+  mode 00755
+end
+
+execute homebrew_go do
   not_if { File.exist? '/usr/local/bin/brew' }
 end
 
-package 'git'
+package 'git' do
+  not_if "which git"
+end
 
 execute "update homebrew from github" do
   command "/usr/local/bin/brew update || true"
