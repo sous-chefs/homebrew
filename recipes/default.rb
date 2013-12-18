176429eb1438a1472445629e3f6cbe9868fbedca
Seigo Uchida <spesnova@gmail.com>
2013-12-18 11:18:18 +0900

    Rename the attribute for install script.

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@ owner = homebrew_owner
 Chef::Log.debug("Homebrew owner is '#{homebrew_owner}'")
 
 remote_file homebrew_go do
-  source node['homebrew']['install_script_uri']
+  source node['homebrew']['install_script']
   mode 00755
 end
 
