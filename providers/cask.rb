63e54f3b0fa56646e91e64ee4482ba5826e0c5d4
Jeroen Jacobs <git@jeroenj.be>
2014-05-21 21:26:23 +0200

    Use homebrew owner for cask provider

--- providers/cask.rb
+++ providers/cask.rb
@@ -1,3 +1,5 @@
+include ::Homebrew::Mixin
+
 def load_current_resource
   @cask = Chef::Resource::HomebrewCask.new(new_resource.name)
   cask_dir = @cask.name
@@ -14,6 +16,7 @@ action :cask do
   unless @cask.casked
     execute "installing cask #{new_resource.name}" do
       command "/usr/local/bin/brew cask install #{new_resource.name}"
+      user homebrew_owner
       not_if "/usr/local/bin/brew cask list | grep #{new_resource.name}"
     end
   end
@@ -23,6 +26,7 @@ action :uncask do
   if @cask.casked
     execute "uninstalling cask #{new_resource.name}" do
       command "/usr/local/bin/brew cask uninstall #{new_resource.name}"
+      user homebrew_owner
       only_if "/usr/local/bin/brew cask list | grep #{new_resource.name}"
     end
   end
