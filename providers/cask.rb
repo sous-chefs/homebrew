e5fc6d8700c6d3237a128da925d06da691d0227a
jtimberman <joshua@opscode.com>
2014-07-25 17:04:59 -0600

    Fixes #43, #44, #45
    Closes #43:
    
    * Use `brew cask list` on the cask to determine if it is installed. If
      it not, the command returns a non-zero exit status. The `exitstatus
      == 0` will return true or false, so we don't need an `if` block.
    * Additionally, the `not_if` aren't required since we are only
      configuring the resource if `@cask.casked` is `false`.
    
    Closes #44:
    
    * Add `:install` and `:uninstall` actions. For compatibility, `:cask`
      and `:uncask` use these respectively.
    
    Closes #45:
    
    * Add `default_action` method to set the default action if it's defined.
    * Print warning messages that Chef versions less than 10.10, when
      `default_action` was introduced, will be unsupported in the next
      major release of the cookbook.

--- providers/cask.rb
+++ providers/cask.rb
@@ -1,33 +1,35 @@
+require 'chef/mixin/shell_out'
+include Chef::Mixin::ShellOut
 include ::Homebrew::Mixin
 
 def load_current_resource
   @cask = Chef::Resource::HomebrewCask.new(new_resource.name)
-  cask_dir = @cask.name
-
-  Chef::Log.debug("Checking whether we've already installed cask #{new_resource.name}")
-  if ::File.directory?("/opt/homebrew-cask/Caskroom/#{cask_dir}")
-    @cask.casked true
-  else
-    @cask.casked false
-  end
+  Chef::Log.debug("Checking whether #{new_resource.name} is installed")
+  @cask.casked shell_out("/usr/local/bin/brew cask list #{new_resource.name}").exitstatus == 0
 end
 
-action :cask do
+action :install do
   unless @cask.casked
     execute "installing cask #{new_resource.name}" do
       command "/usr/local/bin/brew cask install #{new_resource.name}"
       user homebrew_owner
-      not_if "/usr/local/bin/brew cask list | grep #{new_resource.name}"
     end
   end
 end
 
-action :uncask do
+action :uninstall do
   if @cask.casked
     execute "uninstalling cask #{new_resource.name}" do
       command "/usr/local/bin/brew cask uninstall #{new_resource.name}"
       user homebrew_owner
-      only_if "/usr/local/bin/brew cask list | grep #{new_resource.name}"
     end
   end
+end
+
+action :cask do
+  action_install
+end
+
+action :uncask do
+  action_uninstall
 end
