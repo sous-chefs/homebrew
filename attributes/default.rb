68064658f4bc4576fe37fa1983b775084f8c12a3
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-01-22 14:34:54 -0500

    Add `node.homebrew.taps` attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,3 +23,4 @@ default['homebrew']['owner'] = nil
 default['homebrew']['auto-update'] = true
 default['homebrew']['casks'] = []
 default['homebrew']['formulas'] = node['homebrew']['formula'] || []
+default['homebrew']['taps'] = []
