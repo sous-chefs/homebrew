697185573821a3bdc20ab10c88ae25d299a41679
Brendan Germain <Brendan.Germain@nasdaqomx.com>
2015-04-07 11:22:37 -0400

    added options to cask

--- providers/cask.rb
+++ providers/cask.rb
@@ -17,7 +17,7 @@ end
 action :install do
   unless @cask.casked
     execute "installing cask #{new_resource.name}" do
-      command "/usr/local/bin/brew cask install #{new_resource.name}"
+      command "/usr/local/bin/brew cask install #{new_resource.name} #{new_resource.options}"
       user homebrew_owner
     end
   end
