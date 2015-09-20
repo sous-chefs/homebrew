f862f835264ee3a6e4f59a9ad1bf958aed0de355
Tim Smith <tsmith84@gmail.com>
2015-09-20 10:39:20 -0700

    Update copyrights

--- recipes/cask.rb
+++ recipes/cask.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: homebrew
 # Recipes:: cask
 #
-# Copyright 2014, Chef Software, Inc <legal@chef.io>
+# Copyright 2014-2015, Chef Software, Inc <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,9 +2,9 @@
 # Author:: Joshua Timberman (<jtimberman@chef.io>)
 # Author:: Graeme Mathieson (<mathie@woss.name>)
 # Cookbook Name:: homebrew
-# Recipes:: default
+# Recipe:: default
 #
-# Copyright 2011-2013, Chef Software, Inc.
+# Copyright 2011-2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
