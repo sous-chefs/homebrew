32b1cb27164cdf19d52ee6de1b29a6d3b553967c
Tim Smith <tsmith@chef.io>
2015-09-20 19:02:49 -0700

    Remove chef 10 compatibility

--- resources/cask.rb
+++ resources/cask.rb
@@ -1,4 +1,6 @@
 actions :cask, :uncask, :install, :uninstall
+default_action :install
+
 attribute :name,
   name_attribute: true,
   kind_of: String,
@@ -9,14 +11,3 @@ attribute :casked,
 
 attribute :options,
   kind_of: String
-
-if defined?(:default_action)
-  default_action :install
-else
-  Chef::Log.warn("It appears you have Chef version #{Chef::VERSION},")
-  Chef::Log.warn('homebrew_cask resource will remove support for versions of Chef < 10.10 in the next major release of the cookbook')
-  def initialize(*args)
-    super
-    @action = :install
-  end
-end
