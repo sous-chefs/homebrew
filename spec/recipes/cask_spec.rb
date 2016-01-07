f3c38a37d56f625ca6950ac09103bcd8fa20684f
Brian Dwyer <bdwyer@IEEE.org>
2016-01-07 16:22:44 -0500

    Fix the test

--- spec/recipes/cask_spec.rb
+++ spec/recipes/cask_spec.rb
@@ -11,11 +11,7 @@ describe 'homebrew::cask' do
     end
 
     it 'manages the Cask Cache directory' do
-      allow(Dir).to receive(:exist?)
-        .and_call_original
-      allow(Dir).to receive(:exist?)
-        .with('/Library/Caches/Homebrew/Casks')
-        .and_return(true)
+      allow(Dir).to receive(:exist?).with('/Library/Caches/Homebrew').and_return(true)
       expect(chef_run).to create_directory('/Library/Caches/Homebrew/Casks').with(
         user: 'vagrant',
         mode: 00775
@@ -45,11 +41,7 @@ describe 'homebrew::cask' do
     end
 
     it 'manages the Cask Cache directory' do
-      allow(Dir).to receive(:exist?)
-        .and_call_original
-      allow(Dir).to receive(:exist?)
-        .with('/Library/Caches/Homebrew/Casks')
-        .and_return(true)
+      allow(Dir).to receive(:exist?).with('/Library/Caches/Homebrew').and_return(true)
       expect(chef_run).to create_directory('/Library/Caches/Homebrew/Casks').with(
         user: 'alaska',
         mode: 00775
