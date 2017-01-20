Brantone/chef-homebrew

Latest brew install needs extended sudo rights
Signed-off-by: Antek S. Baranski <antek.baranski@gmail.com>

recipes/default.rb
@@ -33,11 +33,32 @@ remote_file homebrew_go do
   not_if { ::File.exist? '/usr/local/bin/brew' }
 end
 
-execute 'install homebrew' do
-  command homebrew_go
-  environment lazy { { 'HOME' => ::Dir.home(homebrew_owner), 'USER' => homebrew_owner } }
-  user homebrew_owner
-  not_if { ::File.exist? '/usr/local/bin/brew' }
+begin
+  template '/etc/sudoers.d/homebrew_sudo' do
+    source 'homebrew_sudo.erb'
+    variables (
+      lazy {
+        { :user => homebrew_owner, :hostname => node['hostname'],
+          :commands => node['homebrew']['sudo']['commands'] }
+      }
+    )
+    action :create
+    mode '00644'
+    user 'root'
+    group 'wheel'
+    not_if { (::File.exist? '/usr/local/bin/brew') || node['homebrew']['sudo']['commands'].empty? }
+  end
+
+  execute 'install homebrew' do
+    command "#{homebrew_go} < /dev/null"
+    environment lazy { { 'HOME' => ::Dir.home(homebrew_owner), 'USER' => homebrew_owner } }
+    user homebrew_owner
+    not_if { ::File.exist? '/usr/local/bin/brew' }
+  end
+ensure
+  file '/etc/sudoers.d/homebrew_go' do
+    action :delete
+  end
 end
 
 if node['homebrew']['auto-update']

sous-chefs/homebrew

recipes/default.rb
@@ -30,11 +30,34 @@ remote_file homebrew_go do
   not_if { ::File.exist? '/usr/local/bin/brew' }
 end
 
-execute 'install homebrew' do
-  command homebrew_go
-  environment lazy { { 'HOME' => ::Dir.home(homebrew_owner), 'USER' => homebrew_owner } }
-  user homebrew_owner
-  not_if { ::File.exist? '/usr/local/bin/brew' }
+directory '/etc/sudoers.d' do
+  mode '00644'
+  owner 'root'
+  group 'wheel'
+  action :create
+end
+
+begin
+  template '/etc/sudoers.d/homebrew' do
+    source 'homebrew_sudo.erb'
+    variables(lazy { { 'user' => homebrew_owner, 'hostname' => node['hostname'], 'commands' => node['homebrew']['sudo']['commands'] } })
+    action :create
+    mode '00644'
+    user 'root'
+    group 'wheel'
+    not_if { (::File.exist? '/usr/local/bin/brew') || node['homebrew']['sudo']['commands'].empty? }
+  end
+
+  execute 'install homebrew' do
+    command "#{homebrew_go} < /dev/null"
+    environment lazy { { 'HOME' => ::Dir.home(homebrew_owner), 'USER' => homebrew_owner } }
+    user homebrew_owner
+    not_if { ::File.exist? '/usr/local/bin/brew' }
+  end
+ensure
+  file '/etc/sudoers.d/homebrew' do
+    action :delete
+  end
 end
 
 execute 'set analytics' do