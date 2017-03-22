19850648117459fb50d8195263964fc95c9d8ab9
Tim Smith <tsmith@chef.io>
2017-03-21 21:09:18 -0700

    Use standardize Apache 2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'homebrew'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Install Homebrew and includes resources for working with taps and casks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.0'
--- test/cookbooks/test/metadata.rb
+++ test/cookbooks/test/metadata.rb
@@ -1,5 +1,5 @@
 name 'test'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Tests homebrew'
 version '0.1.0'
 depends 'build-essential'
