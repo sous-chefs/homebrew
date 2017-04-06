ba96832f2b7d3e322137265875d208fb3523e1ce
Tim Smith <tsmith@chef.io>
2017-04-05 23:45:01 -0700

    Require 12.5+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,4 +10,4 @@ supports         'mac_os_x'
 
 source_url 'https://github.com/chef-cookbooks/homebrew'
 issues_url 'https://github.com/chef-cookbooks/homebrew/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.5'
