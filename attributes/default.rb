1f71b0ce7c6d4e9347205a6dd4e05039de9505cb
Tim Smith <tsmith@chef.io>
2017-04-05 23:34:22 -0700

    Convert cask to a custom resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@
 # Cookbook:: homebrew
 # Attributes:: default
 #
-# Copyright:: 2011-2016, Chef Software, Inc.
+# Copyright:: 2011-2017, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- resources/cask.rb
+++ resources/cask.rb
@@ -1,14 +1,31 @@
-actions :cask, :uncask, :install, :uninstall
-default_action :install
+property :name, String, regex: /^[\w-]+$/, name_property: true
+property :options, String
 
-attribute :name,
-  name_attribute: true,
-  kind_of: String,
-  regex: /^[\w-]+$/
+include ::Homebrew::Mixin
 
-attribute :options,
-  kind_of: String
+action :install do
+  execute "installing cask #{new_resource.name}" do
+    command "/usr/local/bin/brew cask install #{new_resource.name} #{new_resource.options}"
+    user homebrew_owner
+    environment lazy { { 'HOME' => ::Dir.home(homebrew_owner), 'USER' => homebrew_owner } }
+    not_if { new_resource.casked? }
+  end
+end
 
-def casked?
-  shell_out('/usr/local/bin/brew cask list 2>/dev/null').stdout.split.include?(name)
+action :uninstall do
+  execute "uninstalling cask #{new_resource.name}" do
+    command "/usr/local/bin/brew cask uninstall #{new_resource.name}"
+    user homebrew_owner
+    environment lazy { { 'HOME' => ::Dir.home(homebrew_owner), 'USER' => homebrew_owner } }
+    only_if { new_resource.casked? }
+  end
+end
+
+alias_method :action_cask, :action_install
+alias_method :action_uncask, :action_uninstall
+
+action_class.class_eval do
+  def casked?
+    shell_out('/usr/local/bin/brew cask list 2>/dev/null').stdout.split.include?(name)
+  end
 end
