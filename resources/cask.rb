600c9cbfa84e723fff461247a306f7d48425c082
Tim Smith <tsmith@chef.io>
2018-01-13 17:53:42 -0800

    Cookstyle fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/cask.rb
+++ resources/cask.rb
@@ -18,7 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-property :name, String, regex: %r(^[\w/-]+$), name_property: true # ~FC108
+property :name, String, regex: %r{^[\w/-]+$}, name_property: true # ~FC108
 property :options, String
 property :install_cask, [true, false], default: true
 
