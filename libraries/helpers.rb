5a16ea71cc4c1097de9baf7e41d72f4eb9064ce3
Tim Smith <tsmith@chef.io>
2018-03-08 10:33:08 -0800

    Update library header
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -2,9 +2,9 @@
 # Author:: Joshua Timberman (<jtimberman@chef.io>)
 # Author:: Graeme Mathieson (<mathie@woss.name>)
 # Cookbook:: homebrew
-# Library:: homebrew_mixin
+# Library:: helpers
 #
-# Copyright:: 2011-2017, Chef Software, Inc.
+# Copyright:: 2011-2018, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
