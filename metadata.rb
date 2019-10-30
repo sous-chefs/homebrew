88ea2d1a0b43b2b840bf0629719724042ef64541
Tim Smith <tsmith84@gmail.com>
2019-10-30 13:54:46 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,7 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Install Homebrew and includes resources for working with taps and casks'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '5.0.8'
-recipe           'homebrew', 'Install Homebrew'
 supports         'mac_os_x'
 
 source_url 'https://github.com/chef-cookbooks/homebrew'
--- test/cookbooks/test/metadata.rb
+++ test/cookbooks/test/metadata.rb
@@ -2,6 +2,6 @@ name 'test'
 license 'Apache-2.0'
 description 'Tests homebrew'
 version '0.1.0'
-depends 'build-essential'
+
 depends 'homebrew'
 depends 'ssh_known_hosts'
