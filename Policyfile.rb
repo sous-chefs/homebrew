f4a84d4e2e8bc6f17d7d4c5c439be4d29fbc34db
Dan Webb <dan.webb@damacus.io>
2026-06-30 10:45:50 +0100

    chore: use Policyfile dependency resolution (#218)

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,13 +1,10 @@
-# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
+# frozen_string_literal: true
 
 name 'homebrew'
 
-# Where to find external cookbooks:
 default_source :supermarket
 
-# run_list: chef-client will run these recipes in the order specified.
 run_list 'test::default'
 
-# Specify a custom source for a single cookbook:
 cookbook 'homebrew', path: '.'
 cookbook 'test', path: './test/cookbooks/test'
